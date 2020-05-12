4fea6c600340aa805356360d9b23891ab1c15edd
COulson Kgathi <ckgathi@gmail.com>
2020-05-12 09:01:56 +0200

    ad middlewares

--- covid_19/settings.py
+++ covid_19/settings.py
@@ -68,6 +68,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
     'edc_dashboard.middleware.DashboardMiddleware',
 ]
 
